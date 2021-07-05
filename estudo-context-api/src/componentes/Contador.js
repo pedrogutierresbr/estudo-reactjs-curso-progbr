aea3f2a34d8aa3342db347781ff1f7b893322f57
Pedro Gutieres <pedro-gabriel1@hotmail.com>
2021-07-05 16:18:02 -0300

    arrumado bugs dos arquivos existentes

--- estudo-context-api/src/componentes/Contador.js
+++ estudo-context-api/src/componentes/Contador.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { ContadorContexto } from "./ContadorContexto";
 
 function Contador(props) {
