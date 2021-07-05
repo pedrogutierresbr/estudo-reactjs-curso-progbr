e80e8cf677115684a05a781554af5c29894eb6dc
Pedro Gutieres <pedro-gabriel1@hotmail.com>
2021-07-05 16:14:07 -0300

    arrumado diretorio estudo-context-api

--- estudo-context-api/src/componentes/Cabecalho.js
+++ estudo-context-api/src/componentes/Cabecalho.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { ContadorContexto } from "./ContadorContexto";
 
 function Cabecalho(props) {
-    const [contagem, setContagem] = useContext(ContadorContexto);
+    const [contagem] = useContext(ContadorContexto);
     return (
         <div className="cabecalho">
             <h3>Contador</h3>
