66661faa9af239f926697987315f2be430c2e101
Pedro Gutieres <pedro-gabriel1@hotmail.com>
2021-07-07 19:28:50 -0300

    ajustado codigo para trabalhar com mais de 1 estado

--- redux-study/index.js
+++ redux-study/index.js
@@ -1,5 +1,8 @@
 const redux = require("redux");
-const createStore = redux.createStore;
+const createStore = redux.createStore; // 'use quando tiver apenas um estado'
+const combineReducers = redux.combineReducers; //'use quando tiver mais de um estado'
+
+// Criando actions e reducer para o contador
 
 // Criando actions
 const incrementAction = (value) => {
@@ -23,8 +26,48 @@ function counterReducer(state = 0, action) {
     }
 }
 
-// Criando a Store
-const store = createStore(counterReducer);
+// // Criando a Store
+// const store = createStore(counterReducer);
+// console.log(store.getState()); // nessa linha ele imprimi o estado incial
+
+// // Sempre que algo no meu store mudar, eu imprimo a mudança na tela
+// store.subscribe(() => {
+//     console.log(store.getState());
+// });
+
+// // Aqui ele faz a função de dispatch, leva a ação até o reducer (o garçom fazendo analogia ao exemplo do restaurante)
+// store.dispatch(incrementAction());
+// store.dispatch(incrementAction(1));
+// store.dispatch(incrementAction(4));
+// store.dispatch(incrementAction(2));
+
+//=====================================================================
+
+// Criando actions e reducer para a lista
+
+// Criando actions
+const addItemAction = (item) => {
+    return { type: "ADD_ITEM", payload: item };
+};
+
+// Criando Reducer
+const listReducer = (state = [], action) => {
+    switch (action.type) {
+        case "ADD_ITEM":
+            return [...state, action.payload];
+
+        default:
+            return state;
+    }
+};
+
+//Criando objeto para receber mais de um reducer
+const allReducers = combineReducers({
+    counter: counterReducer,
+    list: listReducer,
+});
+
+const store = createStore(allReducers);
 console.log(store.getState()); // nessa linha ele imprimi o estado incial
 
 // Sempre que algo no meu store mudar, eu imprimo a mudança na tela
@@ -33,7 +76,7 @@ store.subscribe(() => {
 });
 
 // Aqui ele faz a função de dispatch, leva a ação até o reducer (o garçom fazendo analogia ao exemplo do restaurante)
-store.dispatch(incrementAction());
+store.dispatch(addItemAction("item 1"));
 store.dispatch(incrementAction(1));
 store.dispatch(incrementAction(4));
 store.dispatch(incrementAction(2));
