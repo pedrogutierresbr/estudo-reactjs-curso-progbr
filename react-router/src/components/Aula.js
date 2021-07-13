920cc6998b859ceff35bc8d4684e742c552911cb
Pedro Gutieres <pedro-gabriel1@hotmail.com>
2021-07-13 15:01:03 -0300

    criado um custom hooks e substituido utilizacao do useState, useEffect e useParams separados

--- react-router/src/components/Aula.js
+++ react-router/src/components/Aula.js
@@ -1,17 +1,16 @@
 import data from "../Data";
 
 import { useParams } from "react-router-dom";
-import React, { useState, useEffect } from "react";
+import React from "react";
+
+function useAula() {
+    const id = useParams().id;
+    // eslint-disable-next-line eqeqeq
+    return data.filter((aula) => aula.id == id)[0];
+}
 
 export default function Aula(props) {
-    const params = useParams();
-
-    const [aula, setAula] = useState({});
-
-    useEffect(() => {
-        // eslint-disable-next-line eqeqeq
-        setAula(data.filter((aula) => aula.id == params.id)[0]);
-    }, [params]);
+    const aula = useAula();
 
     return (
         <div className="page">
