8fbb67adfbc178b27b4a50dd15ced3da4533f456
Pedro Gutieres <pedro-gabriel1@hotmail.com>
2021-07-13 14:56:20 -0300

    utilizado hooks params para filtrar objeto e mostrar conteudo de acordo com a rota requisitada

--- react-router/src/components/Aula.js
+++ react-router/src/components/Aula.js
@@ -1,10 +1,22 @@
-import React from "react";
+import data from "../Data";
+
+import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 
 export default function Aula(props) {
+    const params = useParams();
+
+    const [aula, setAula] = useState({});
+
+    useEffect(() => {
+        // eslint-disable-next-line eqeqeq
+        setAula(data.filter((aula) => aula.id == params.id)[0]);
+    }, [params]);
+
     return (
         <div className="page">
-            <h1>Title</h1>
-            <h3>Desc</h3>
+            <h1>{aula.title}</h1>
+            <h3>{aula.desc}</h3>
         </div>
     );
 }
